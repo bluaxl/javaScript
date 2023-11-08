772650c995ff7b0633ce210c8d4935f605691e53
bluaxl <bluaxl102@gmail.com>
2023-11-08 17:50:45 -0500

    proyecto 8

--- Proyecto8/script.js
+++ Proyecto8/script.js
@@ -64,7 +64,7 @@ function cuentaRegresiva(tiempoFaltante, reloj, mensaje){
     },1000);
 };
 
-cuentaRegresiva('NOV 08 2023 17:34:00', 'cuentaRegresiva','!Feliz Navidad¡')
+cuentaRegresiva('NOV 08 2023 17:50:00', 'cuentaRegresiva','!Feliz Navidad¡')
 
 function play(){
     botonAudio.play();
